bf9e83668b59671056ce534cc1c9e6cdc3e7ece5
Tehsein Firoze <73216889+tfiroze@users.noreply.github.com>
2024-04-11 17:09:28 +0100

    Initializing package.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analytics_store',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'sklearn'
+    ],
+    author='Tehsein',
+    description='Utility functions for data visualization and model evaluation',
+    url='https://github.com/yourusername/analytics_store'
+)
